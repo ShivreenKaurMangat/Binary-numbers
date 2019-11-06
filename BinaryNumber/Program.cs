077da14af688bdafbf555b1ee8ad834dab847ab0
shivreenmangat08@gmail.com <shivreenmangat08@gmail.com>
2019-11-06 13:37:34 -0600

    Add project files.

---
+++ BinaryNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToBinary(18));
+            Console.ReadLine();
+        }
+
+        static string ConvertToBinary(int n)
+        {
+            string result = "";
+            while (n > 0)
+            {
+                int reminder = n % 2;
+                result = reminder + result;
+                n = n / 2;
+            }
+            return result;
+        }
+    }
+}
